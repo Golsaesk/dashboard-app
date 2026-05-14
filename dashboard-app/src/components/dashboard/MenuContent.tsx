8ca637fd0c9c40725ab08c596e9b19e74d2d9587
Golsa Eskandari <golsa@Golsas-MacBook-Air.local>
2026-05-14 19:02:03 +0330

    fixed some bugs

--- dashboard-app/src/components/dashboard/MenuContent.tsx
+++ dashboard-app/src/components/dashboard/MenuContent.tsx
@@ -1,5 +1,6 @@
 import { MenuItem } from '@/data/menu'
 import { useRouter } from 'next/navigation'
+
 type props = {
   items: MenuItem[]
   isOpen: boolean
@@ -15,16 +16,11 @@ export default function MenuContent({ items, onClose, isOpen }: props) {
   }
   return (
     <>
-      {/* overlay */}
       <div
         onClick={onClose}
-        className={`fixed inset-0 bg-black/40 transition-opacity duration-300 ${isOpen ? 'opacity-100' : 'pointer-events-none opacity-0'} `}
+        className={`fixed inset-0 bg-black/80 transition-opacity duration-500 ${isOpen ? 'opacity-500' : 'pointer-events-none opacity-0'} `}
       />
-
-      {/* menu panel */}
-      <div
-        className={`fixed top-0 right-0 h-full w-64 transform bg-white shadow-lg transition-transform duration-300 ease-in-out ${isOpen ? 'translate-x-0' : 'translate-x-full'} `}
-      >
+      <div className={` ${isOpen ? 'translate-x-0' : 'translate-x-full'} `}>
         <div className="space-y-3 p-4">
           {items.map((item) => {
             const Icon = item.icon
@@ -33,10 +29,10 @@ export default function MenuContent({ items, onClose, isOpen }: props) {
               <div
                 key={item.path}
                 onClick={() => handleNavigate(item.path)}
-                className="flex cursor-pointer items-center gap-3 rounded p-3 hover:bg-gray-100"
+                className="flex cursor-pointer items-center gap-3 rounded p-3 text-base transition-all duration-500 hover:bg-gray-500 hover:text-lg"
               >
-                <Icon size={18} />
-                <span>{item.lable}</span>
+                <Icon size={18} className="text-white" />
+                <span className="text-white">{item.lable}</span>
               </div>
             )
           })}
--- dashboard-app/src/data/menu.ts
+++ dashboard-app/src/data/menu.ts
@@ -1,7 +1,7 @@
-import { LucideIcon } from "lucide-react";
+import { LucideIcon } from 'lucide-react'
 
-export type MenuItem ={
-    lable:string;
-    path:string;
-    icon: LucideIcon
+export type MenuItem = {
+  lable: string
+  path: string
+  icon: LucideIcon
 }
