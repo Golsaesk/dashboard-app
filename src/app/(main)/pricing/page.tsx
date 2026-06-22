a12dd5015b292e38eb8a24dd327f2f10a69755ca
Golsa Eskandari <golsa@Golsas-MacBook-Air.local>
2026-06-22 15:20:58 +0330

    last changes

--- src/app/(main)/pricing/page.tsx
+++ src/app/(main)/pricing/page.tsx
@@ -1,6 +1,6 @@
 import Pricing from "@/components/dashboard/Pricing";
 
-
+// import Pricing from "@/components/dashboard/Pricing";
 export default function PricingPage() {
     return (
         <div>
